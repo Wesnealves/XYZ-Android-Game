5714de1353213fa5f0a62641777597ee392be924
Wesne Alves <wesne123@hotmail.com>
2020-06-08 17:47:53 -0300

    Modified: New player move && player jump. It's smoother now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,30 +7,79 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    
+    // Movimento e Pulo
+
     public float speedPlayer;
-      public float jumpVelocity;
+    public float jumpVelocity;
+
+
+    private bool isMovingZ = false;
+    private bool isMovingX = true;
+    private bool canJump = true;
+
+    
     Rigidbody playerRigidbody;
 
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
     }
+    
 
     private void Update()
     {
-        playerRigidbody.AddForce(new Vector3(speedPlayer,0,0) * Time.deltaTime, ForceMode.Impulse);
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerRigidbody.AddForce(new Vector3(jumpVelocity/2,jumpVelocity,0) * jumpVelocity);
-        }
+        PlayerMove();
+        PlayerJump(); // Funcionou melhor em Update do que em Fixed Update;
 
-        
-
-
-
-       
     }
 
+    public void PlayerMove()
+    {
+        // Verificação de qual eixo irá se 'movimentar'.
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isMovingX = true;
+            isMovingZ = false;
+        }else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isMovingZ = true;
+            isMovingX = false;
+        }
+
+        // Eixo que irá se movimentar.
+        if (isMovingX)
+        {
+            this.transform.Translate(new Vector3(1, 0, 0) * speedPlayer * Time.deltaTime);
+        }else if (isMovingZ)
+        {
+            this.transform.Translate(new Vector3(0, 0, 1) * speedPlayer * Time.deltaTime);
+        }
+    }
+
+    public void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isMovingX && canJump)
+        {
+            // Pula no eixo X
+            canJump = false;
+            playerRigidbody.AddForce(new Vector3(jumpVelocity / 2, jumpVelocity, 0) * jumpVelocity);
+            StartCoroutine(PlayerCanJump());
+
+        }else if (Input.GetKeyDown(KeyCode.Space) && isMovingZ && canJump)
+        {
+            // Pula no eixo Z
+            canJump = false;
+            playerRigidbody.AddForce(new Vector3(0, jumpVelocity, jumpVelocity / 2) * jumpVelocity);
+            StartCoroutine(PlayerCanJump());
+        }
+    }
+
+    IEnumerator PlayerCanJump()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canJump = true;
+
+    }
 
 }
